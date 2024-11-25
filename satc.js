20da366ff87870076ede493315794cced1d8041a
atquebral <ana.quebral@edu.sait.ca>
2024-11-24 23:50:28 -0700

    started javascript
    -added bg in css
    -added and tested event listeners

---
+++ satc.js
@@ -0,0 +1,34 @@
+intro.addEventListener('click',function(){
+    console.log('testing the intro');
+
+})
+
+season1.addEventListener('click',function(){
+    console.log('Mr. Big was introduced here, yuck');
+
+})
+
+season2.addEventListener('click',function(){
+    console.log('Carrie and Big breakup part 1');
+
+})
+
+season3.addEventListener('click',function(){
+    console.log('Aiden, Steve, Trey: yay, yay, boo');
+
+})
+
+season4.addEventListener('click',function(){
+    console.log('Carrie cheats on Aiden with Big ugh');
+
+})
+
+season5.addEventListener('click',function(){
+    console.log('shortest season bc SJP was pregnant irl haha');
+
+})
+
+season5.addEventListener('click',function(){
+    console.log('Goes to paris only to end up with Big: big boo');
+
+})
