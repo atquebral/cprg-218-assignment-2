451d384662fc22217c951929b18c56100d6f11cc
atquebral <ana.quebral@edu.sait.ca>
2024-11-27 02:00:54 -0700

    refine html and css
    - simplified paragraph and header id's
    - stylized and edited padding for header (bottns and h1)
    - imported fonts
    -stylized image

--- satc.js
+++ satc.js
@@ -8,7 +8,6 @@ intro.addEventListener('click',function(){
     document.body.classList.remove("season5");
     document.body.classList.remove("season6");
     document.body.classList.add("intro");
-    document.getElementById("seasonp").innerHTML = "";
     document.getElementById("seasontitle").innerHTML = "";
 })
 
@@ -24,7 +23,7 @@ season1.addEventListener('click',function(){
     document.body.classList.remove("season6");
     document.body.classList.add("season1");
     document.getElementById("introp").innerHTML = "";
-    seasonp.innerHTML = "This is the description for season 1";
+    description.innerHTML = "This is the description for season 1";
 })
 
 season2.addEventListener('click',function(){
@@ -39,7 +38,7 @@ season2.addEventListener('click',function(){
     document.body.classList.remove("season6");
     document.body.classList.add("season2");
     document.getElementById("introp").innerHTML = "";
-    seasonp.innerHTML = "This is the description for season 2";
+    description.innerHTML = "This is the description for season 2";
 })
 
 season3.addEventListener('click',function(){
@@ -53,7 +52,7 @@ season3.addEventListener('click',function(){
     document.body.classList.remove("season6");
     document.body.classList.add("season3");
     document.getElementById("introp").innerHTML = "";
-    seasonp.innerHTML = "This is the description for season 3";
+    description.innerHTML = "This is the description for season 3";
 })
 
 season4.addEventListener('click',function(){
@@ -67,7 +66,7 @@ season4.addEventListener('click',function(){
     document.body.classList.remove("season6");
     document.body.classList.add("season4");
     document.getElementById("introp").innerHTML = "";
-    seasonp.innerHTML = "This is the description for season 4";
+    description.innerHTML = "This is the description for season 4";
 })
 
 season5.addEventListener('click',function(){
@@ -81,7 +80,7 @@ season5.addEventListener('click',function(){
     document.body.classList.remove("season6");
     document.body.classList.add("season5");
     document.getElementById("introp").innerHTML = "";
-    seasonp.innerHTML = "This is the description for season 5";
+    description.innerHTML = "This is the description for season 5";
 })
 
 season6.addEventListener('click',function(){
@@ -95,5 +94,5 @@ season6.addEventListener('click',function(){
     document.body.classList.remove("season1");
     document.body.classList.add("season6");
     document.getElementById("introp").innerHTML = "";
-    seasonp.innerHTML = "This is the description for season 6";
+    description.innerHTML = "This is the description for season 6";
 })
